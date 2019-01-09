d7eafc7ec4812d0f3282675b9aa9e0f66f7f7669
Pawan Kumar Singh <pawan4444@gmail.com>
2019-01-09 06:48:08 +0530

    Add: Remove duplicate integers from an unsorted array of integers

---
+++ miscellaneous_problem/remove_duplicate_unsorted_array.cpp
@@ -0,0 +1,45 @@
+// File: remove_duplicate_unsorted_array.cpp
+// Created on: 6th Jan 2019
+// Updated on: 6th Jan 2019
+// Author: Pawan Kumar Singh [pawan@codingbull.com]
+// http://www.codingbull.com
+//
+// This source code is the part of free tutorial series provided by http://www.codingbull.com and
+// it can be used/distributed without any restriction.
+//
+// Input: 1, 2, 5, 1, 7, 2, 4, 2
+// Output: 1, 2, 5, 7, 4
+// Desciption: Remove duplicate integers from an unsorted array containing integer values
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> remove_duplicate(vector<int> arr) {
+    vector<int> return_values;
+    unordered_map<int, int> map;
+    for (int temp : arr) {
+      if (map.find(temp) == map.end()) {
+          //Not found in map
+          return_values.push_back(temp);
+          map[temp] = temp;
+      }
+    }
+    return return_values;
+}
+
+void print_array(vector<int> arr) {
+
+    for (int temp: arr) {
+        cout << temp << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+     vector<int> value {1, 2, 5, 1, 7, 2, 4, 2};
+     vector unique_values = remove_duplicate(value);
+     print_array(unique_values);
+}
